0c3def67e2d4c087aa39a75ca48f2b3dba09bd58
louiebb <244429290@qq.com>
2018-08-19 20:44:38 +0800

    头部center

--- src/api/getnav.php
+++ src/api/getnav.php
@@ -1,12 +1,9 @@
 <?php
     include 'connect.php';
-
     //数据库脚本
-    $sql = "select name from t_province";
-
-    //执行
+    $sql = "SELECT  id ,`value`,more,pubclass from t_nav where `enable` = true";
+    // include 'connect.php';  
     $res = $mydb->getdata($sql);
     $data =  json_encode($res,JSON_UNESCAPED_UNICODE);
- 
-    echo $data;
+    echo $data;  
 ?>
--- src/js/top.js
+++ src/js/top.js
@@ -7,6 +7,7 @@ var vvtop = {
     business:'.pub-buscen',
     server:'.pub-server',
     follow:'.pub-followus',
+    pubtopright:'.pub-top-right',
     init(){
         this.currentpro = $(this.currentpro);
         this.province = $(this.province);
@@ -16,21 +17,73 @@ var vvtop = {
         this.business = $(this.business);
         this.server = $(this.server);
         this.follow = $(this.follow);
+        this.pubtopright = $(this.pubtopright);
         this.initProvince(this);
+        this.initPubNav(this);
 
     },
     initProvince(self){
         $.ajax({
             url:'../api/getProvince.php',
-            success:function(text){
-                var content = JSON.parse(text).map(x=>`<li>${x.name}</li>`);
-                self.province.html(content).on('click','li',function(e){
-                    var $lis = $(this).siblings('li');
-                    $lis.removeClass('active');
-                    this.classList.add('active');
-                    self.currentpro.text(this.innerText);
-                });
+            success:function(txt){
+                if(txt){
+                    var content = JSON.parse(txt).map(x=>`<li>${x.name}</li>`);
+                    self.province.html(content).on('click','li',function(e){
+                        var $lis = $(this).siblings('li');
+                        $lis.removeClass('active');
+                        this.classList.add('active');
+                        self.currentpro.text(this.innerText);
+                    });
+                }
             }
+        });
+    },
+    initPubNav(self){
+        let navPromise = new Promise((resolve,reject)=>{
+            $.ajax({
+                url:'../api/getnav.php',
+                success:function(data){
+                    resolve(data);
+                }
+            });
+        });
+
+        navPromise.then(data=>{
+            let content = JSON.parse(data).map(x=>`
+                        <dd class="${x.more} pub-${x.pubclass}">
+                        <div data-id="${x.id}">
+                            <a href="#">${x.value}</a>
+                        </div>
+                        <ul>
+                        </ul>
+                    </dd>
+                    `).join('');
+            $(content).appendTo(self.pubtopright);
+        });
+
+        self.pubtopright.on('mouseenter','dd',function(){    
+            self.initnavVal($(this).children('div').attr('data-id'),$(this).children('ul'));
+        });
+    },
+    initnavVal(navid,ul){
+        let navValPro = new Promise((re,rc)=>{
+            $.ajax({
+                url:'../api/getnavValue.php?id='+navid,
+                success:function(data){
+                    re(JSON.parse(data));
+                }
+            });
+        });
+        navValPro.then(data=>{
+            let content ;
+            if(data.length && data[0].navid !=5){
+                content =data.map(x=>`<li><a href="${x.url}">${x.value}<a/></li>`).join('');
+            }else if(data.length){
+                content =`<p>${data[0].value}</p>
+                            <p>${data[1].value}</p>
+                            <img src="../img/top/${data[2].value}"/>`;
+            }
+            ul.html(content);
         });
     }
 }
