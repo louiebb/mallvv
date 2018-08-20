7d14278b0884fd5e3b358d2da0fd56ea86f16fb7
louiebb <244429290@qq.com>
2018-08-20 22:51:45 +0800

    完成头部与底部

--- src/api/getdata.php
+++ src/api/getdata.php
@@ -1,7 +1,25 @@
 <?php
     include 'connect.php';
-    $sql  = isset($_GET['sql'])?$_GET['sql']:'select * from t_test';
-        switch()
+    $type  = isset($_GET['type'])?$_GET['type']:'none';
+    $id = isset($_GET['id'])?$_GET['id']:null;
+    $sql = 'select * from t_test';
+    switch($type){
+        case 'rec':
+        $sql = "select id,title,url from t_recomm";
+        break;
+        case 'recval':
+        $sql = "select  * from t_recomm_val where rec_id = $id";
+        break;
+        case 'bubmat':
+        $sql = "select * from t_bubmat";
+        break;
+        case 'jjjf':
+        $sql = "select * from t_jjjf";
+        break;
+        
+        default:
+        break;
+    }
     $res = $mydb->getdata($sql);
     $data =  json_encode($res,JSON_UNESCAPED_UNICODE);
     echo $data;  
--- src/js/top.js
+++ src/js/top.js
@@ -9,7 +9,11 @@ var vvtop = {
     follow:'.pub-followus',
     pubtopright:'.pub-top-right',
     pubrecmenu:'.pub-rec-menu',
+    pubnavfirst:'.pub-nav-first',
+    colblcok:'.pub-nav-second .col-blcok',
+    pubnavsecond:'.pub-nav-second',
     init(){
+        //获取节点
         this.currentpro = $(this.currentpro);
         this.province = $(this.province);
         this.login = $(this.login);
@@ -20,37 +24,43 @@ var vvtop = {
         this.follow = $(this.follow);
         this.pubtopright = $(this.pubtopright);
         this.pubrecmenu = $(this.pubrecmenu);
+        this.pubnavfirst = $(this.pubnavfirst);
+        this.colblcok = $(this.colblcok);
+        this.pubnavsecond = $(this.pubnavsecond);
+        
+        let self = this;
+        //绑定事件
+        this.pubnavfirst.on('mouseenter','li',function(){
+            if(this.className){
+                self.initSecondNav(this.className);
+                self.pubnavsecond.css('display','block');
+            }
+        });
+        this.pubnavsecond.on('mouseleave',function(){
+            self.pubnavsecond.css('display','none');            
+        })
+
+
+        //初始化
         this.initProvince(this);
         this.initPubNav(this);
+        this.initRecommend();
+
 
     },
     initProvince(self){
-        $.ajax({
-            url:'../api/getProvince.php',
-            success:function(txt){
-                if(txt){
-                    var content = JSON.parse(txt).map(x=>`<li>${x.name}</li>`);
-                    self.province.html(content).on('click','li',function(e){
-                        var $lis = $(this).siblings('li');
-                        $lis.removeClass('active');
-                        this.classList.add('active');
-                        self.currentpro.text(this.innerText);
-                    });
-                }
-            }
-        });
+        self.ajaxProm('../api/getProvince.php','',function(txt){
+            var content = JSON.parse(txt).map(x=>`<li>${x.name}</li>`);
+            self.province.html(content).on('click','li',function(e){
+                var $lis = $(this).siblings('li');
+                $lis.removeClass('active');
+                this.classList.add('active');
+                self.currentpro.text(this.innerText);
+            });
+        })
     },
     initPubNav(self){
-        let navPromise = new Promise((resolve,reject)=>{
-            $.ajax({
-                url:'../api/getnav.php',
-                success:function(data){
-                    resolve(data);
-                }
-            });
-        });
-
-        navPromise.then(data=>{
+        self.ajaxProm('../api/getnav.php','',function(data){
             let content = JSON.parse(data).map(x=>`
                         <dd class="${x.more} pub-${x.pubclass}">
                         <div data-id="${x.id}">
@@ -61,22 +71,16 @@ var vvtop = {
                     </dd>
                     `).join('');
             $(content).appendTo(self.pubtopright);
-        });
-
-        self.pubtopright.on('mouseenter','dd',function(){    
-            self.initnavVal($(this).children('div').attr('data-id'),$(this).children('ul'));
-        });
-    },
-    initnavVal(navid,ele){
-        let navValPro = new Promise((re,rc)=>{
-            $.ajax({
-                url:'../api/getnavValue.php?id='+navid,
-                success:function(data){
-                    re(JSON.parse(data));
-                }
+            self.pubtopright.on('mouseenter','dd',function(){    
+                self.initnavVal($(this).children('div').attr('data-id'),$(this).children('ul'));
             });
         });
-        navValPro.then(data=>{
+
+        
+    },
+    initnavVal(navid,ele){
+        this.ajaxProm('../api/getnavValue.php?id='+navid,'',function(data){
+            data = JSON.parse(data);
             let content ;
             if(data.length && data[0].navid !=5){
                 content =data.map(x=>`<li><a href="${x.url}">${x.value}<a/></li>`).join('');
@@ -88,22 +92,57 @@ var vvtop = {
             ele.html(content);
         });
     },
-    initRecommend(self){
-        $.ajax({
-            url:'../api/getdata.php?sql=select id,title,contetn from t_recommend',
-            success:function(txt){
-                if(txt){
-                    var content = JSON.parse(txt).map(x=>`<li>${x.name}</li>`);
-                    self.province.html(content).on('click','li',function(e){
-                        var $lis = $(this).siblings('li');
-                        $lis.removeClass('active');
-                        this.classList.add('active');
-                        self.currentpro.text(this.innerText);
+    initRecommend(){
+            let self = this;
+            this.ajaxProm('../api/getdata.php?type=rec','',function(x){
+                    let  content =JSON.parse(x).map(j=>{
+                    return `<div class="pub-menu-list">
+                    <div><a href="${j.url}">${j.title}</a></div>
+                    <ul class="clearfix" data-id="${j.id}">
+                    </ul></div>`;
+                }).join('');
+                    self.pubrecmenu.html(content);
+                    let  $uls = self.pubrecmenu.children().children('ul');
+                    $uls.each(function(x,y){
+                        self.ajaxProm('../api/getdata.php?type=recval&id='+y.dataset.id,'',function(k){
+                            let content = JSON.parse(k).map(z=>`<li><a href="${z.url}">${z.val}</a></li>`).join('');
+                            $(y).html(content);
+                        });
                     });
+            });
+        
+    },
+    initSecondNav(tab){
+        let self = this;
+        this.ajaxProm('../api/getdata.php?type='+tab,'',function(data){
+             data = JSON.parse(data);
+             let content = ``;
+             data.forEach(x=>{
+                content += `<div class="pub-nav-wrap" style="display: block;">
+                <a class="t" href="${x.t_url}" target="_blank">${x.title}</a>
+                <ul class="server-list">`;
+                x.content.split(',').forEach(y=>{
+                    content += `<li class="l hl"><a href="#" target="_blank">${y}</a></li>`
+                });
+                content +=` </ul></div>`;
+             });
+            self.colblcok.html(content);
+        })
+    },
+    ajaxProm(url,data,callback){
+        let Prom = new Promise((resolve,reject)=>{
+            $.ajax({
+                url:url,
+                success:function(data){
+                    resolve(data);
                 }
-            }
+            });
+        });
+        Prom.then(data=>{
+            callback(data);
         });
     }
+    
 }
 
 
